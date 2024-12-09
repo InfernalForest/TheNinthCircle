453d5f4ca3fdaaee3811002aa94fa31a7d746f3c
d b <david@rizkocircle.com>
2024-12-09 15:12:02 -0800

    Merging sharpshooter and telemetry changes to main branch, added sharpshooter component and variable to first person character

--- Source/TheNinthCircle/Public/WeaponData.h
+++ Source/TheNinthCircle/Public/WeaponData.h
@@ -6,16 +6,13 @@
 #include "Engine/DataAsset.h"
 #include "WeaponData.generated.h"
 
-/**
- * 
- */
 
-USTRUCT(BlueprintType)
-struct FWeaponInformation {
+UCLASS()
+class THENINTHCIRCLE_API UWeaponData : public UPrimaryDataAsset
+{
 	GENERATED_BODY()
 
-public:
-
+private:
 	UPROPERTY()
 	int32 BaseDamage;
 
@@ -23,56 +20,46 @@ public:
 	int32 MagazineCapacity;
 
 	UPROPERTY()
+	int32 RPM;
+	
+	UPROPERTY()
 	float ReloadSpeed;
 
 	UPROPERTY()
-	int32 RPM;
-
-	UPROPERTY()
 	FString WeaponName;
-};
-
-
-UCLASS()
-class THENINTHCIRCLE_API UWeaponData : public UPrimaryDataAsset
-{
-	GENERATED_BODY()
-
-	UPROPERTY()
-	FWeaponInformation Telemetry;
 
 	UFUNCTION(BlueprintCallable)
 	void initWeaponData(FString name, int32 damage, float reloadtime, int32 rof, int32 capacity) {
-		Telemetry.WeaponName = name;
-		Telemetry.BaseDamage = damage;
-		Telemetry.ReloadSpeed = reloadtime;
-		Telemetry.RPM = rof;
-		Telemetry.MagazineCapacity = capacity;
+		WeaponName = name;
+		BaseDamage = damage;
+		ReloadSpeed = reloadtime;
+		RPM = rof;
+		MagazineCapacity = capacity;
 	}
 	
 	UFUNCTION(BlueprintCallable)
 	FString getWeaponName() {
-		return Telemetry.WeaponName;
+		return WeaponName;
 	}
 
 	UFUNCTION(BlueprintCallable)
 	int getBaseDamage() {
-		return Telemetry.BaseDamage;
+		return BaseDamage;
 	}
 
 	UFUNCTION(BlueprintCallable) 
 	int getMagazineCapacity() {
-		return Telemetry.MagazineCapacity;
+		return MagazineCapacity;
 	}
 
 	UFUNCTION(BlueprintCallable)
 	float getReloadSpeed() {
-		return Telemetry.ReloadSpeed;
+		return ReloadSpeed;
 	}
 
 	UFUNCTION(BlueprintCallable)
 	int getRPM() {
-		return Telemetry.RPM;
+		return RPM;
 	}
 };
 
